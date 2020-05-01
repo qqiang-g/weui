0e9c81e93efb27752af77deb7752e4296e87b6d8
GQiangQ <740407980@qq.com>
2020-05-01 21:00:02 +0800

    init parcel

--- components/button/button.js
+++
@@ -1,5 +0,0 @@
-Vue.component('we-button',{
-    template:`
-        <button class="we-button">button</button>
-    `
-})
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Button from 'components/button/Button'
+new Vue({
+    el: "#app",
+    components:{
+        Button,
+    }
+})
