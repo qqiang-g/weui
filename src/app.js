43099f0a93878c684c4352690c709000de855293
GQiangQ <740407980@qq.com>
2020-05-06 22:21:52 +0800

    button修改

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import Button from './Button.vue'
-Vue.components('we-button',Button)
+Vue.component('we-button',Button)
 new Vue({
     el:'#app',
-    data:{
-        message:'oh yeah!!!'
-    }
-})
+    
+}) 
