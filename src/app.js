61f2c3c241b4dce54b0dedf4efff503b0e10a3a8
GQiangQ <740407980@qq.com>
2020-05-08 12:43:04 +0800

    单元测试button的iconPorsition

--- src/app.js
+++ src/app.js
@@ -16,30 +16,69 @@ new Vue({
 import chai from 'chai'
 const expect = chai.expect
 //单元测试
+//icon
 {
     const Constructor = Vue.extend(Button)
     const button = new Constructor({
         propsData:{
             icon:'setting',
-        },
-        solt:'ninin'
+        }
     })
     button.$mount()
     let useElement = button.$el.querySelector('use')
     let href = useElement.getAttribute('xlink:href')
     expect(href).to.eq('#w-setting')
+    button.$el.remove()
+    button.$destroy()
 }
+//loading
 {
     const Constructor = Vue.extend(Button)
     const button = new Constructor({
         propsData:{
             icon:'setting',
             loading:true
-        },
-        solt:'ninin'
+        }
     })
     button.$mount()
     let useElement = button.$el.querySelector('use')
     let href = useElement.getAttribute('xlink:href')
     expect(href).to.eq('#w-loading')
+    button.$el.remove()
+    button.$destroy()
 }
+//iconPositon
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon:'setting',
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData:{
+            icon:'setting',
+            iconPosition:'right'
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    button.$el.remove()
+    button.$destroy()
+}
+//
