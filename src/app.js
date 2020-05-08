ead22f93f69ea8ec9dcd609e77fab9c776710b68
GQiangQ <740407980@qq.com>
2020-05-08 13:03:59 +0800

    添加spies,并修改button的click

--- src/app.js
+++ src/app.js
@@ -14,88 +14,92 @@ new Vue({
     }
 }) 
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
 const expect = chai.expect
 //单元测试
 //icon
 {
     const Constructor = Vue.extend(Button)
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData:{
             icon:'setting',
         }
     })
-    button.$mount()
-    let useElement = button.$el.querySelector('use')
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
     let href = useElement.getAttribute('xlink:href')
     expect(href).to.eq('#w-setting')
-    button.$el.remove()
-    button.$destroy()
+    vm.$el.remove()
+    vm.$destroy()
 }
 //loading
 {
     const Constructor = Vue.extend(Button)
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData:{
             icon:'setting',
             loading:true
         }
     })
-    button.$mount()
-    let useElement = button.$el.querySelector('use')
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
     let href = useElement.getAttribute('xlink:href')
     expect(href).to.eq('#w-loading')
-    button.$el.remove()
-    button.$destroy()
+    vm.$el.remove()
+    vm.$destroy()
 }
 //iconPositon
 {
     const div = document.createElement('div')
     document.body.appendChild(div)
     const Constructor = Vue.extend(Button)
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData:{
             icon:'setting',
         }
     })
-    button.$mount(div)
-    let svg = button.$el.querySelector('svg')
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
     let {order} = window.getComputedStyle(svg)
     expect(order).to.eq('1')
-    button.$el.remove()
-    button.$destroy()
+    vm.$el.remove()
+    vm.$destroy()
 }
 {
     const div = document.createElement('div')
     document.body.appendChild(div)
     const Constructor = Vue.extend(Button)
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData:{
             icon:'setting',
             iconPosition:'right'
         }
     })
-    button.$mount(div)
-    let svg = button.$el.querySelector('svg')
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
     let {order} = window.getComputedStyle(svg)
     expect(order).to.eq('2')
-    button.$el.remove()
-    button.$destroy()
+    vm.$el.remove()
+    vm.$destroy()
 }
-//
+//点击
 {
     const Constructor = Vue.extend(Button)
-    const we_button = new Constructor({
+    const vm = new Constructor({
         propsData:{
             icon:'setting',
             iconPosition:'right',
         }
     })
-    we_button.$mount()
-    we_button.$on('click',function(){
-        expect(1).to.eq(1)
+    vm.$mount()
+    let spy = chai.spy(function(){
+
     })
-    let button = we_button.$el
+    vm.$on('click',spy)
+    let button = vm.$el
     button.click()
-    we_button.$el.remove()
-    we_button.$destroy()
+    expect(spy).to.have.been.called()
+    vm.$el.remove()
+    vm.$destroy()
 }
